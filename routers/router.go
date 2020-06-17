22796dff3c3cf8922a53eb8f157625f6d4ce7d49
lihuan <494397353@qq.com>
2020-06-17 11:58:21 +0800

    set cors

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"net/http"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 
@@ -18,9 +19,39 @@ import (
 	"go-eth/routers/api/v1"
 )
 
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		var filterHost = [...]string{"http://localhost.*", "https://*.perceive.top"}
+		// filterHost 做过滤器，防止不合法的域名访问
+		var isAccess = false
+		for _, v := range filterHost {
+			match, _ := regexp.MatchString(v, origin)
+			if match {
+				isAccess = true
+			}
+		}
+		if isAccess {
+			// 核心处理方式
+			c.Header("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
+			c.Set("content-type", "application/json")
+		}
+		//放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.JSON(http.StatusOK, "Options Request!")
+		}
+
+		c.Next()
+	}
+}
+
 // InitRouter initialize routing information
 func InitRouter() *gin.Engine {
 	r := gin.New()
+	r.Use(CorsMiddleware())
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
